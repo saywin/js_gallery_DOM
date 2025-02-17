ec1314b0d920d4c74d946917c0aa47d730afb069
Artem <stilon670@gmail.com>
2025-02-17 14:08:45 +0200

    make build

---
+++ index.f468dc35.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list"),t=document.querySelector("#largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");t.src=r.href});
+//# sourceMappingURL=index.f468dc35.js.map
